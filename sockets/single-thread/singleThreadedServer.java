47da5b229abd476d73ac859411a6c96f0eba4f64
jyooDev <gina07099@gmail.com>
2024-09-13 18:51:10 -0500

    singlethreaded

--- sockets/single-thread/singleThreadedServer.java
+++ sockets/single-thread/singleThreadedServer.java
@@ -1,74 +1,97 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
-class SingleThreadedServer()
+class SingleThreadedServer
 {
+    final String DISCONNECT_MSG = "disconnect";
+    
     ServerSocket server;
     InputStream in;
     OutputStream out;
+    Socket client;
+    Scanner sc; 
 
     public SingleThreadedServer(int port){
         try{
             server = new ServerSocket(port);
         }
         catch(IOException e){
-            ioException.printStackTrace();
+            e.getMessage();
         }
+        sc = new Scanner(System.in);
     }
 
-    public void run(){
-        
+    public void run(){       
         try{
             while(true){
-                //Listen for a connection and create client socket
-                System.out.println("Waiting for connection...");
-                Socket client = server.accept();
-                System.out.println("Connection established from " + client.getInetAddress().getHostName());
-                
-                //Read data from client
-                in = client.getInputStream();
-                out = client.getOutputStream();
-
-                // Read message from client
-                printMessage();
-
+                //open connection
+                if (client == null){
+                    connect();
+                }
                 // Send response to client
-                sendMessage();
-
-                // Close connection
-                client.close();
+                sendMessage();                
+                // Read message from client
+                String client_message = getClientMessage();
+                if(client_message.toLowerCase().equals(DISCONNECT_MSG))
+                {
+                    // Close connection
+                    System.out.println("Disconnect from client.");
+                    client.close();
+                    client = null;
+                }
             }
         }catch(IOException e){
-            ioException.printStackTrace();
+            e.getMessage();
+        }finally {
+            sc.close(); // Close the scanner when the server terminates
         }
     }
 
-    void printMessage(){
+    void connect(){
+        //Listen for a connection and create client socket
+        System.out.println("Waiting for connection...");
         try {
+            client = server.accept();            
+        } catch (IOException e) {
+        }
+        System.out.println("Connection established from " + client.getInetAddress().getHostName());
+    }
+
+    String getClientMessage(){
+        String message = "";
+        try {
+            in = client.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String message = reader.readLine();
+            message = reader.readLine();
             System.out.println("Received from client: " + message);
         } catch (IOException e) {
-            e.printStackTrace();
+            e.getMessage();
         }
+        return message;
     }
 
     void sendMessage(){
-        Scanner sc = new Scanner(System.in);
         System.out.print("Enter message to send to client: ");
-            String message = sc.nextLine();
-        try {    
+        String message = sc.nextLine();
+        try {
+            out = client.getOutputStream();
             PrintWriter writer = new PrintWriter(out, true);
             writer.println(message);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            e.getMessage();
         }
     }
 
     public static void main(String args[])
 	{
-		SingleThreadedServer client = new SingleThreadedServer(6868);
+        if(args.length != 1){
+            System.out.println("You need to pass only one argument, port.");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+		SingleThreadedServer client = new SingleThreadedServer(port);
 		client.run();
 	}
 }
